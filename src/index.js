40cef73bab0ba0b35f6a92541b0ddc719620cbc7
Roshane Miller <millerroshane44@gmail.com>
2022-03-10 18:52:22 -0500

    added animations fixed height issue
    added animations with gsap and changed covers height from 100 vh to a min height of 100vh

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+gsap.from(".bg-img-cont", {
+  duration: 1,
+  opacity: 0,
+});
+gsap.from(".bg-img-cont", {
+  duration: 1.5,
+  y: "50%",
+  ease: "bounce",
+});
+gsap.from(".section-cont", {
+  duration: 1,
+  x: "100%",
+});
+gsap.from(".acordion-title", {
+  duration: 1,
+  opacity: 0,
+  delay: 1,
+});
+gsap.to(".arrow-down", {
+  duration: 0.5,
+  opacity: 1,
+  delay: 1.5,
+});
+
 let acordionBody = document.querySelector("acordion-body");
 let accordions = document.querySelectorAll(".acordion");
 let downArrow = document.querySelectorAll(".arrow-down");
